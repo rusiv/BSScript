28561b3ff0017a6d95cb45a4247e596184a41905
rusiv <4ever.87@mail.ru>
2017-11-10 14:07:53 +0400

    add settings for protect server

--- BSScript.py
+++ BSScript.py
@@ -1,12 +1,38 @@
 import sublime
 import sublime_plugin
 import os
-import subprocess
+from ctypes import *
+import time
 
+def getVersion(bsccPath):
+	bscc = open(bsccPath, "rb")
+	data = bscc.read().decode("utf-8", "ignore")
+	bscc.close()
+	fullVersion = data[data.find('http://bss.bssys.com/code/') + 26:data.find('/bscc.exe')]
+	return fullVersion.split('.')[1]
 
+def getSettings():
+	activeWindow = sublime.active_window()
+	projectPath = activeWindow.extract_variables()["project_path"]
+	workingDir = projectPath
+	if os.path.exists(projectPath + "\\bank"):
+		workingDir = projectPath + "\\bank"
+	fileFullPath = activeWindow.extract_variables()["file"]
+	global_settings = sublime.load_settings("BSScript.sublime-settings")
+	version = getVersion(workingDir + "\\exe\\bscc.exe")
+	protectServer = global_settings.get("protect_server_" + version, "")
+	protectServerAlias = global_settings.get("protect_server_alias_" + version, "")
+	return {
+		"projectPath": projectPath,
+		"working_dir": workingDir,
+		"fileFullPath": fileFullPath,
+		"bllFullPath": projectPath + "\\user\\" + activeWindow.extract_variables()["file_base_name"] + ".bll",
+		"protect_server": protectServer,
+		"protect_server_alias": protectServerAlias
+	}
 
 class bsscriptCompileCommand(sublime_plugin.WindowCommand):
-	def run(self):
+	def run(self):		
 		activeWindow = sublime.active_window()
 		activeWindow.run_command("save")
 
@@ -16,15 +42,25 @@ class bsscriptCompileEventListeners(sublime_plugin.EventListener):
 		fileExt = activeWindow.extract_variables()["file_extension"].upper()
 		if fileExt != 'BLS' :
 			return
-			
-		projectPath = activeWindow.extract_variables()["project_path"]
-		fileFullPath = activeWindow.extract_variables()["file"]
-		global_settings = sublime.load_settings("BSScript.sublime-settings")
+		settings = getSettings()		
 		activeWindow.run_command("exec", {
-			"working_dir": projectPath,
+			"working_dir": settings.get("working_dir", ""),
 			"path": "exe;system;user",
-			"cmd": ["bscc.exe ", fileFullPath, "-S" + global_settings.get("protect_server", ""), "-A" + global_settings.get("protect_server_alias", ""), "-Tuser"],
+			"cmd": ["bscc.exe ", settings.get("fileFullPath", ""), "-S" + settings.get("protect_server", ""), "-A" + settings.get("protect_server_alias", ""), "-Tuser"],
 			"file_regex": "Program\\s(.*)\\s*.*\\s*.*line is (\\d*)",
-			"quiet": True,
+			"quiet": True
 		})
 		activeWindow.find_output_panel("exec").set_syntax_file("BSScript-build.sublime-syntax")
+
+class bsscriptCompileAndTestCommand(sublime_plugin.WindowCommand):
+	#not working
+	def run(self):
+		activeWindow = sublime.active_window()
+		activeWindow.run_command("save")
+		settings = getSettings()
+		activeWindow.run_command("exec", {
+			"working_dir": settings.get("projectPath", ""),
+			"path": "exe;system;user",
+			"cmd": ["execBLL.exe ", settings.get("bllFullPath", ""), "__test__execute"],
+			"quiet": True
+		})
