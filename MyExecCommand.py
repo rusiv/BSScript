d318eddb84f2f1d6cf11caf1594e99ae1eebb389
rusiv <4ever.87@mail.ru>
2017-12-07 10:59:32 +0400

    fix bll copy

--- MyExecCommand.py
+++ MyExecCommand.py
@@ -4,17 +4,24 @@ from . import CommonFunctions
 import shutil
 import os
 
-class MyExecCommand(ExecCommand):
+class MyExecCommand(ExecCommand):	
 	def __copyBllsInUserPaths(self):
+		activeWindow = sublime.active_window()
 		settings = CommonFunctions.getSettings()
-		if settings.get("version", "") == "15":
-			activeWindow = sublime.active_window()
+		version = settings.get("version", "")
+		if version == "15":			
 			bllFullPath = activeWindow.extract_variables()["file_path"] + "\\" + activeWindow.extract_variables()["file_base_name"] + '.bll'
 			if os.path.exists(bllFullPath):
 				for userPath in settings.get("userPaths", []):
-					shutil.copy2(bllFullPath, userPath)		
-				print(bllFullPath)
+					shutil.copy2(bllFullPath, userPath)
 				os.remove(bllFullPath)
+		elif (version == "17") or (version == "20"):
+			mainUserPath = settings.get("working_dir", "") + "\\user\\"
+			bllFullPath = mainUserPath + activeWindow.extract_variables()["file_base_name"] + '.bll'
+			if os.path.exists(bllFullPath):
+				for userPath in settings.get("userPaths", []):
+					if not os.path.samefile(mainUserPath, userPath):
+						shutil.copy2(bllFullPath, userPath)
 
 	def on_data(self, proc, data):
 		ExecCommand.on_data(self, proc, data)		
