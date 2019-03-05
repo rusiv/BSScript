26bcb5c9fede31b62d6925ef2d66f4c0fb84322d
rusiv <4ever.87@mail.ru>
2019-03-05 11:34:06 +0400

    check on strong dependence on graph

--- Dependencer.py
+++ Dependencer.py
@@ -6,96 +6,101 @@ GREY = 1
 BLACK = 2
 
 class Graph:
-  vertexes = []
-  shortNameIndexList = []
-  
-  def addVertex(self, vertex):
-    self.vertexes.append(vertex)
-    self.shortNameIndexList.append(vertex.shortName)
+	vertexes = []
+	shortNameIndexList = []
+	
+	def addVertex(self, vertex):
+		self.vertexes.append(vertex)
+		self.shortNameIndexList.append(vertex.shortName)
 
-  def getVertexIndexByName(self, name):
-    for i in range(len(self.vertexes)):
-      if self.vertexes[i].name == name:
-        return i
-    return None
+	def getVertexIndexByName(self, name):
+		for i in range(len(self.vertexes)):
+			if self.vertexes[i].name == name:
+				return i
+		return None
 
-  def getVertexIndexByShortName(self, shorName):
-    return self.shortNameIndexList.index(shorName)
+	def getVertexIndexByShortName(self, shorName):
+		return self.shortNameIndexList.index(shorName)
 
-  def getVertexByIndex(self, index):
-    return self.vertexes[index]
+	def getVertexByIndex(self, index):
+		return self.vertexes[index]
 
 class Vertex:
-  def __init__(self, path, name, shortName):
-    self.path = path
-    self.name = name
-    self.fullPath = path + '\\' + name;
-    self.shortName = shortName;
-    
-    self.color = WHITE
-    self.edges = [];
+	def __init__(self, path, name, shortName):
+		self.path = path
+		self.name = name
+		self.fullPath = path + '\\' + name
+		self.shortName = shortName
+		self.cycles = []
+		
+		self.color = WHITE
+		self.edges = [];
 
 class Dependencer:
-  graph = Graph()
-  blsDublicates = []
-  chain = [];
-  cycles = [];
-  compileOrder = [];
+	graph = Graph()
+	blsDublicates = []
+	chain = [];
+	cycles = [];
+	compileOrder = [];
 
-  def __init__(self, blsFolderPath):
-    self.folder = blsFolderPath
-    self.buildGraph()
-    self.buildEdges()
+	def __init__(self, blsFolderPath, onBlsComplete = None):
+		self.folder = blsFolderPath
+		self.onBlsComplete = onBlsComplete
+		self.buildGraph()
+		self.buildEdges()
 
-  def buildGraph(self):
-    bllList = []
+	def buildGraph(self):
+		bllList = []
 
-    for root, dirs, files in os.walk(self.folder):
-      for name in files:
-        if name.upper().endswith('.BLS'):
-          nameWOExt = name[:name.rfind('.')].lower();
+		for root, dirs, files in os.walk(self.folder):
+			for name in files:
+				if name.upper().endswith('.BLS'):
+					nameWOExt = name[:name.rfind('.')].lower();
 
-          if nameWOExt in bllList:
-            self.blsDublicates.append(nameWOExt);
-          else:
-            bllList.append(nameWOExt);
-            
-            vertex = Vertex(root.lower(), name.lower(), nameWOExt)
-            self.graph.addVertex(vertex)
+					if nameWOExt in bllList:
+						blsDublicates.append(nameWOExt);
+					else:
+						bllList.append(nameWOExt);
+						
+						vertex = Vertex(root.lower(), name.lower(), nameWOExt)
+						self.graph.addVertex(vertex)
 
-  def buildEdges(self):
-    for vertex in self.graph.vertexes:
-      dependences = BLSItem(vertex.fullPath, None).dependence
+	def buildEdges(self):
+		for vertex in self.graph.vertexes:
+			dependences = BLSItem(vertex.fullPath, None).dependence
 
-      if dependences:
-        for dependence in dependences:
-          vertex.edges.append(self.graph.getVertexIndexByShortName(dependence.lower()))
+			if dependences:
+				for dependence in dependences:
+					vertex.edges.append(self.graph.getVertexIndexByShortName(dependence.lower()))
 
-  def getCycles(self):
-    if self.blsDublicates:
-      raise Exception('Duplicates detected: ' + ', '.join(self.blsDublicates))
+	def getCycles(self):
+		if self.blsDublicates:
+			raise Exception('Duplicates detected: ' + ', '.join(self.blsDublicates))
 
-    for vertex in self.graph.vertexes:
-      self.dfs(vertex)
+		for vertex in self.graph.vertexes:
+			self.dfs(vertex)
 
-    return self.cycles
+		return self.cycles
 
-  def dfs(self, vertex):
-    if vertex.color == BLACK:
-      return None
-    
-    self.chain.append(vertex.name)
-    vertex.color = GREY
-    for key in vertex.edges:
-      vertex2 = self.graph.getVertexByIndex(key)
-      if vertex2.color == WHITE:
-        self.dfs(vertex2)
-      elif vertex2.color == GREY:
-        self.cycles.append(' -> '.join(self.chain) + ' -> ' + vertex2.name)
-        #break
-    vertex.color = BLACK
-    self.chain.pop();
-    self.compileOrder.append(vertex.fullPath)
+	def dfs(self, vertex):
+		if vertex.color == BLACK:
+			return None
+		
+		self.chain.append(vertex.name)
+		vertex.color = GREY		
+		for key in vertex.edges:
+			vertex2 = self.graph.getVertexByIndex(key)
+			if vertex2.color == WHITE:
+				self.dfs(vertex2)
+			elif vertex2.color == GREY:				
+				vertex.cycles.append(self.chain)
+				self.cycles.append(' -> '.join(self.chain) + ' -> ' + vertex2.name)
+				#break
+		vertex.color = BLACK
+		if self.onBlsComplete:
+			self.onBlsComplete(vertex)
+		self.chain.pop()
+		self.compileOrder.append(vertex.fullPath)
 
-  def getOrder(self):
-    return self.compileOrder
+	def getOrder(self):
+		return self.compileOrder
